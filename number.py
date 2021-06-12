d20dabd90a19b89a96244ae8b039cddde8f9536e
tsuka414 <keita04141537@icloud.com>
2021-06-13 04:56:22 +0900

    modify

--- number.py
+++ number.py
@@ -1,15 +1,28 @@
-print("This is hit number game")
+def number_hit_game():
 
-answer = 1
-guess = input()
-trial = 1
+    answer = 10
+    
+    print("This is hit number game")
+    print("Please input number >>>")
+    print(answer)
 
-while answer != guess:
-  trial += 1
-  print("You input wrong number")
-  print("next challenge?")
-  guess = input()
-else:
-  print("You good")
-  print("trial_number is")
-  print(trial)
+    a = input()
+    guess = int(a)
+    print(type(guess))
+
+    trial = 1
+    
+    while answer != guess:
+        trial += 1
+        print("You input wrong number")
+        print("next challenge?")
+        a = input()
+        guess = int(a) 
+    else:
+        print("You good")
+        print("trial_number is")
+        text = ("{trial} + time")
+        print(text)
+
+if __name__ == "__main__":
+    number_hit_game()
