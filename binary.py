c0cf3e82ec0a133f94b4db5ddc520157f9b8f8ca
tsuka414 <keita04141537@icloud.com>
2021-07-14 00:58:57 +0900

    change number

--- binary.py
+++ binary.py
@@ -2,3 +2,14 @@
 n = int(input())
 
 print(int(f'{n}', 2))
+
+# 10進数を2,8,16進数に変換する
+x = 10
+print(bin(x))
+print(oct(x))
+print(hex(x))
+
+# 2,8,16進数を10進数に変換する
+print(int('10100', 2))
+print(int('24', 8))
+print(int('14', 16))
